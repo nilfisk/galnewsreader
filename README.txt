5d620b7a7d303efa185d479d1afd910c1901fb7d
Sammy Fischer <sammy@cosmic-bandito.com>
2014-12-25 17:30:12 +0100

    added zok

--- README.txt
+++ README.txt
@@ -14,10 +14,16 @@ GalNewsReader - a pseudo news reader for Frontier's Elite:Dangerous Galnews webs
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-Version: 1.2
+Version: 2.0
+The source file should always be in the 7z archive! Please contact me if it is missing
 
 Changelog :
+	2.0
+	  new:
+	  * added Szokorad's Galnet Archive as alternative feed
+	  * set the galnet archive as default feed as Frontier took
+		down the official page
+	1.2
 	  new:
 	  * the output is writen to a file ./galnews
 	  fixes:
@@ -26,12 +32,18 @@ Changelog :
 	  * fix for the weird spelling of "transmission"
 	  * fix for the weird :: in some headlines
 
+For old users : 
+	As of Dec.24th Frontier seems to have taken down the Galnet News Page, so a new (default!) news feed has been added, namely Cmdr Szokorad galnetarchive which can be found at 
+		http://galnetarchive.blogspot.com/feeds/posts/default
+	Note that the parameters have been modified too!
+ 	  
 Description:
  Small GO Program allowing to read the headlines from Frontier's Elite:Dangerous GalNews
  page or single article summaries. The idea is to have Windows TextToSpeech Engine read
  the news aloud while flying.
  A small powershell script to "say" a string is included but there doesn't seem to be any
  way to call it without windows taking focus from E:D and dropping to the desktop.
+ 
 
 License:
  why GPL3? why *NOT*????
@@ -42,10 +54,23 @@ in order to disable the console window which pops up in Windows when the reader 
          go build -ldflags -H=windowsgui galnewsreader.go
 
 Usage:
- galnewsreader [-item=n]
+ galnewsreader [-item=n] [-zok={0|1}]
+ 
+ if zok=1 (Default bevahiour) then the news from Szokorad's Galnet Archive will be used instead
+ of Frontier's official page.
+ 
  if -item is specified, only the corresponding summary 'n' will be printed out. If it is not
  specified or n equals 0 only the headlines will be printed out.
- if n is negative the full body of the transmission will be printed instead of the summary.
+ For the Frontier Galnet News page :
+	if n is negative the full body of the transmission will be printed instead of the summary.
+ For Szokorad's Galnet Archive :
+	if n is negative the full text of the 100 first entries will be printed.
 
+	
+	
  Using Voice Attack and Luca D's excellent plugin (https://groups.google.com/forum/#!topic/voiceattack/VotudmB84hE) 
  is highly recommended. Use the readConsole example from the plugin's sample profile or the included galnewsreader-sample.vap as template 
+
+ 
+ Enjoy,
+ Sammy Fischer
--- galnewsreader.go
+++ galnewsreader.go
@@ -1,4 +1,4 @@
-/*
+﻿/*
     GalNewsReader - a pseudo news reader for Frontier's Elite:Dangerous Galnews website
     Copyright (C) 2014  Sammy Fischer
 
@@ -27,26 +27,51 @@ import (
 	"regexp"
 	"flag"
 	"os"
+	"html"
 	)
 
-const VERSION = "1.2"
+const VERSION = "2.0"
 
-func itemRequested() int {
-	item := flag.Int("item",0,"headline number for summary, negative headline number for body, 0 for a list of headlines")
-	version := flag.String("help","no","Show the version string")	
-	flag.Parse()
-	if *version != "no" {
-		fmt.Println("galnewsreader copyright 2014 Sammy Fischer\nVersion:"+VERSION+"\n\nhttp://github.com/sammyf/galnewsreader\n\nUseage : galnewsreader -item=n")
-		os.Exit(0)
+/*
+	routines for Szokorad galnetarchive
+	http://galnetarchive.blogspot.com/feeds/posts/default
+*/
+func zok_getArticles(htm string) []map[string]string {
+	weekdays := map[string]string{"Mon":"Monday","Tue":"Tuesday","Wed":"Wednesday","Thu":"Thursday","Fri":"Friday","Sat":"Saturday","Sun":"Sunday"}
+	months := map[string]string{"Jan":"January", "Feb":"February", "Mar":"March", "Apr":"April", "May":"May", "Jun":"June", "Jul":"July", "Aug":"August", "Sep":"September", "Oct":"October", "Nov":"November", "Dec":"December"}
+	re := regexp.MustCompile("<item>(.*?)</item>")
+	entries := re.FindAllStringSubmatch(htm, -1)
+	var articles = make([]map[string]string,0)
+	if len(entries) <= 0 {
+		articles = append(articles, map[string]string{"headline":"no news to show","body":"The newsfeed seems to be currently unavailable"})
+		return articles
 	}
-	return *item
+	for i:=0; i<len(entries); i++ {	
+		re = regexp.MustCompile("<pubDate>(.*?) \\d{2}:\\d{2}:\\d{2} [+-]\\d{4}</pubDate>")
+		date := re.FindStringSubmatch(entries[i][1])[1]
+		re = regexp.MustCompile("([A-Za-z]{3}), \\d{2} ([A-Za-z]{3}) \\d{4}")
+		shorts := re.FindAllStringSubmatch(date, -1)
+		date = strings.Replace(date, shorts[0][1], weekdays[shorts[0][1]], 1);
+		date = strings.Replace(date, shorts[0][2], months[shorts[0][2]], 1);		
+		re = regexp.MustCompile("<title>(.*?)</title>")
+		title := re.FindStringSubmatch(entries[i][1])[1]
+		headline := "stardate: "+date+".\n\n\n\nHeadline:\n"+removeTags(title)+"\n"
+		re = regexp.MustCompile("<description>(.*?)</description>")
+		raw := html.UnescapeString(re.FindStringSubmatch(entries[i][1])[1])
+		re = regexp.MustCompile("<blockquote(.*?)>")
+		raw = re.ReplaceAllLiteralString(raw, "\nQuote:\n ")
+		raw = strings.Replace(raw,"</blockquote>", "\n\nEnd Quote:\n ",-1)
+		body := removeTags(raw)
+		article := map[string]string{"headline":headline,"body":body}
+		articles = append(articles, article)
+	}
+	return articles
 }
 
-func removeTags(htm string) string  {
-	re := regexp.MustCompile("<.*?>")
-	processed := re.ReplaceAllLiteralString(htm,"")
-	return processed
-}
+/*
+	routines for the official Frontier Galnet News page
+*/
+
 
 func getLinkDate(htm string) string {
 	re := regexp.MustCompile("galnet/([\\d]{4}-[\\d]{2}-[\\d]{2})\">")
@@ -102,6 +127,10 @@ func getDetails(nr int, htm string) string {
 	return body	
 }
 
+/*
+	Utilities
+ */
+
 func outputToFile( s string) {
 	b := []byte(s)
 	ioutil.WriteFile("./galnews", b, 0777)
@@ -124,33 +153,83 @@ func retrieveURL( url string) string {
 	return string(buf)
 }
 
-func main() {
-	request := itemRequested()
-	details := 0
-
-	rs :="GALNET News.\n"
-
-	if request < 0 {
-		request = -request
-		details = 1
+func itemRequested() (int,int) {
+	item := flag.Int("item",0,"headline number for summary, negative headline number for body, 0 for a list of headlines")
+	zok := flag.Int("zok",1,"retrieve the articles from Szokorad galnetarchive instead of the official site. note that there are only headlines or full articles. no summaries! A negative number will return 100 full articles.")
+	version := flag.String("help","no","Show the version string")
+	flag.Parse()
+	if *version != "no" {
+		fmt.Println("galnewsreader copyright 2014 Sammy Fischer\nVersion:"+VERSION+"\n\nhttp://github.com/sammyf/galnewsreader\n\nUseage : galnewsreader -item=n -zok=n")
+		os.Exit(0)
 	}
+	return *item, *zok
+}
 
-	htm := retrieveURL("http://www.elitedangerous.com/news/galnet/")
+func removeTags(htm string) string  {
+	re := regexp.MustCompile("<.*?>")
+	processed := re.ReplaceAllLiteralString(htm,"")
+	return processed
+}
 
-	if request != 0 {
-		if details == 0 {
-			rs = rs + "item requested : "+strconv.Itoa(request)+"\n"
-			rs = rs + getArticle(request, htm)
+/*
+	MAIN
+ */
+
+
+func main() {
+	request, zok := itemRequested()
+	details := 0
+	rs := ""
+	if zok==0 {
+		rs = "GALNET News.\n"
+
+		if request < 0 {
+			request = -request
+			details = 1
+		}
+
+		htm := retrieveURL("http://www.elitedangerous.com/news/galnet/")
+
+		if request != 0 {
+			if details == 0 {
+				rs = rs+"item requested : "+strconv.Itoa(request)+"\n"
+				rs = rs+getArticle(request, htm)
+			} else {
+				rs = rs+"full content of item : "+strconv.Itoa(request)+"\n"
+				rs = rs+getDetails(request, htm)
+			}
 		} else {
-			rs = rs + "full content of item : "+strconv.Itoa(request)+"\n"
-			rs = rs + getDetails(request, htm)
+			rs = rs+"Current Headlines : \n\n"
+			headlines := getHeadlines(htm)
+			for i := 0; i < len(headlines); i++ {
+				galDate := getLinkDate(headlines[i])
+				rs = rs+"Headline "+strconv.Itoa(i+1)+".\nStardate "+galDate+".\n"+removeTags(headlines[i])+"\n"
+			}
 		}
 	} else {
-		rs = rs + "Current Headlines : \n\n"
-		headlines := getHeadlines(htm)
-		for i:=0; i<len(headlines); i++ {
-			galDate := getLinkDate(headlines[i])
-			rs = rs + "Headline "+strconv.Itoa(i+1)+".\nStardate "+galDate+".\n"+removeTags(headlines[i])+"\n"
+		rs = "Zsokorad Galnet News - The fastest news in the Galaxy\n\n"
+		maxresults := "10"
+		if request < 0 {
+			maxresults = "100"
+		}
+		htm := retrieveURL("http://galnetarchive.blogspot.com/feeds/posts/default?max-results="+maxresults+"&alt=rss")
+		articles := zok_getArticles(htm)
+		if request == 0 {
+			rs = rs+"Current Headlines : \n\n"
+			for i := 0; i < len(articles); i++ {
+				rs = rs+"item "+strconv.Itoa(i+1)+":\n"+articles[i]["headline"]+"\n\n\n"
+			}
+		} else if request > 0 {
+			if request > len(articles) {
+				rs = rs+"This item does not exist"
+			} else {
+				rs = rs+articles[request-1]["headline"]+"\n\n"+articles[request-1]["body"]+"\n\n\n"
+			}
+		} else {
+			fmt.Println(articles)
+			for i := 0; i < len(articles); i++ {
+rs=rs+"\n\n\nitem"+strconv.Itoa(i+1)+":\n"+articles[i]["headline"]+"\n\n"+articles[i]["body"]+"\n\n\n"
+			}
 		}
 	}
 	rs = rs + "\nEnd of Stream.\n"
